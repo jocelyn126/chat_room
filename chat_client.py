09f62120966c65c3803b2ec445086ec1f36307e3
Jocelyn126 <baccy126@126.com>
2020-06-21 11:34:54 +0800

    add administrator

--- chat_client.py
+++ chat_client.py
@@ -3,55 +3,54 @@ chat room 客户端
 发送请求， 获取结果
 """
 import sys
-from socket import *
 from multiprocessing import Process
+from socket import *
 
-ADDR=("119.3.161.197",8000)
+ADDR=("127.0.0.1",6007)
 
 
 def login(sock):
     while True:
-        name=input("Name:")
+        name=input("请输入姓名：")
         msg="L "+name
         sock.sendto(msg.encode(),ADDR)
-        result,add=sock.recvfrom(1024)
-        if result.decode()=="OK":
-            print("进入聊天室")
-            return name
+        data,address=sock.recvfrom(128)
+        if data.decode()=="Fail":
+            print("用户名已存在")
         else:
-            print("该用户已存在")
+            print("欢迎进入聊天室")
+            return name
 
-#接收消息
-def receive_msg(sock):
+
+def receive_message(sock):
     while True:
-        data,addr=sock.recvfrom(4096)
-        msg="\n"+data.decode()+"\n发言："
-        print(msg,end="") #打印之后不换行
+        data,address=sock.recvfrom(4096)
+        msg = "\n" + data.decode() + "\n发言："
+        print(msg,end="")
 
-#发送消息
-def send_msg(sock,name):
+
+def chat(sock,name):
     while True:
         try:
-            message=input("发言：")
+            content=input("发言：")
         except:
-            message="quit"
-        if message =="quit":
+            content="quit"
+        if content=="quit":
             msg="Q "+name
             sock.sendto(msg.encode(),ADDR)
             sys.exit("您已退出聊天室")
-        msg="C %s %s"%(name,message)
-        sock.sendto(msg.encode(),ADDR) #发送消息给服务器
+        msg="C %s %s"%(name,content)
+        sock.sendto(msg.encode(),ADDR)
 
 
 def main():
     sock=socket(AF_INET,SOCK_DGRAM)
     sock.bind(("0.0.0.0",6009))
-    name=login(sock) #name 进入聊天室 拿login的返回值
-    #创建子进程
-    p=Process(target=receive_msg,args=(sock,))
-    p.daemon=True #这是个进程属性， 设置成True就是父进程退出带走子进程
+    name=login(sock) #name作为返回值这一步很难想到
+    p=Process(target=receive_message,args=(sock,))
+    p.daemon=True
     p.start()
-    send_msg(sock,name) #发送消息
+    chat(sock,name)
 
 if __name__ == '__main__':
     main()
--- chat_server.py
+++ chat_server.py
@@ -1,87 +1,67 @@
 """
 author: Jocelyn
 email:baccy126@126.com
-time:2020-6-13
+time:2020-6-14
 env:Python3.6
 socket and Process exercise
 """
-from multiprocessing import Process
+from multiprocessing import Process,Queue
 from socket import *
-#服务器地址
 HOST="0.0.0.0"
-PORT=8000
+PORT=6007
 ADDR=(HOST,PORT)
 
-#存储用户{name:address..}
-user={}
+user_info={}
 
-#处理登录请求
-def do_login(sock,name,address):
-    if name in user:
-        sock.sendto(b"Fail",address)
+def do_login(sock, name, address):
+    if name in user_info:
+        sock.sendto(b"Fail",address) #这边自己写的时候没有想到要发信号
         return
     else:
         sock.sendto(b"OK", address)
-        #通知其他人
-        msg="欢迎%s进入聊天室"%name
-        for i in user:
-            sock.sendto(msg.encode(),user[i])
-        #存储用户
-        user[name]=address
+        msg="欢迎%s 进入聊天室"%name#格式化的字符串不能直接encode
+        for key in user_info:
+            sock.sendto(msg.encode(),user_info[key])
+        user_info[name]=address
 
 
-#转发消息
-def do_chat(sock,name,message):
-    msg="%s :%s"%(name,message)
-    for i in user or "管理" in name:
-        if i != name:
-            sock.sendto(msg.encode(),user[i])
+def do_chat(sock, name, contant):
+    msg="%s :%s"%(name,contant)
+    for key in user_info or "管理" in name:
+        if name!=key:
+            sock.sendto(msg.encode(),user_info[key])
 
 
-#退出
 def do_quit(sock, name):
-    del user[name] #先删除这个人， 就不用跳过这个人去通知其他人了
-    msg = "%s 退出聊天室" % name
-    for i in user:
-        sock.sendto(msg.encode(), user[i])
+    if name in user_info:
+        del user_info[name]
+    msg="%s已经退出聊天室"%name
+    for key in user_info:
+        sock.sendto(msg.encode(),user_info[key])
 
 def do_request(sock):
-    # 循环接收用户请求
     while True:
-        # 接收用户请求(所有客户端的所有请求都向这里发送)
-        data, addr = sock.recvfrom(1024)
-        tmp = data.decode().split(" ", 2)
-        # 根据请求调用模块
-        if tmp[0] == "L":
-            # tmp-->['L','name']
-            do_login(sock, tmp[1], addr)
-        elif tmp[0] == "C":
-            # tmp-->['C','name','content']
-            do_chat(sock, tmp[1], tmp[2])
-        elif tmp[0] == "Q":
-            # tmp-->['Q','name']
-            do_quit(sock, tmp[1])
+        data, address = sock.recvfrom(1024)
+        temp = data.decode().split(" ", 2)
+        if temp[0] == "L":
+            do_login(sock, temp[1], address)
+        elif temp[0] == "C":
+            do_chat(sock, temp[1], temp[2])
+        elif temp[0] == "Q":
+            do_quit(sock, temp[1])
 
-#程序启动函数
 def main():
-    sock=socket(AF_INET,SOCK_DGRAM) #UDP
+    sock=socket(AF_INET,SOCK_DGRAM)
     sock.bind(ADDR)
-    do_request(sock)
-
-    #创建进程
     p=Process(target=do_request,args=(sock,))
     p.daemon=True
     p.start()
-
-    #父进程 发送管理员消息
     while True:
         content=input("管理员消息：")
-        if content=="quit": #操作整个服务端程序的口令， 是quit整个服务器全部结束
+        if content=="quit":
             break
         msg="C 管理员消息 "+content
-        sock.sendto(msg.encode(),("127.0.0.1",6009)) #发消息给子进程
-
-
+        sock.sendto(msg.encode(),("127.0.0.1",6009))
 
 if __name__ == '__main__':
     main()
